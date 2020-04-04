ef72d47b82a026f959abc66b570864626857debb
Siyanda Khanyile <Siyanda.Khanyile@silverbridge.co.za>
2020-04-04 21:16:34 +0200

    Add project files.

---
+++ EasyShopperQA/PageObjects/Login.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyShopperQA.TestCase
+{
+	public class Login
+	{
+
+	}
+}
---
+++ EasyShopperQA/TestCases/Base/TC_SignUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyShopperQA.TestCases.Base
+{
+	public class TC_SignUp
+	{
+
+	}
+}
